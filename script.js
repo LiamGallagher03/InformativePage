638387862a1df19451a720fb579b688959dbdf20
Liam Gallagher <71441726+LiamGallagher03@users.noreply.github.com>
2021-09-23 13:06:11 -0400

    9/23/2021

---
+++ script.js
@@ -0,0 +1,24 @@
+let total = 0
+let $product = document.getElementById("product")
+let $total = document.getElementById("total")
+let $quantity = document.getElementById("quantity")
+
+$product.onchange = calcTotal
+
+function calcTotal() {
+    let productPrice
+    if ($product.value == "p1")
+        productPrice = 89.99
+    else if ($product.value == "p2")
+        productPrice = 349.95
+    else if ($product.value == "p3") {
+        productPrice = 2.75
+    }
+    let quantity = $quantity.value;
+    let total = quantity * productPrice
+    console.log(productPrice, quantity)
+    $total.value = total
+
+}
+
+calcTotal()
